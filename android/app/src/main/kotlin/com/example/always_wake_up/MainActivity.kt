3c009b5523fe982623a0631c493fc786a8b5d151
Mathis Fouques <mathisfouques@MacBook-Pro-de-Mathis-2.local>
2023-05-06 16:36:46 +0200

    Initial message

---
+++ android/app/src/main/kotlin/com/example/always_wake_up/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.always_wake_up
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/alarm.dart
@@ -0,0 +1,11 @@
+class Alarm {
+  final DateTime date;
+  final String message;
+  final String soundPath;
+
+  Alarm({
+    required this.date,
+    required this.message,
+    required this.soundPath,
+  });
+}
